c3dd09dedc395a7d0dbc66aaecd9dd421cc23749
noriyuki.matsumoto <noriyuki.matsumoto@uzabase.com>
2018-12-14 22:14:46 +0900

    initial

---
+++ pages/about/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about-page">
+    <h1>This is for U-30 first rollout.</h1>
+    <p>This is not only trial but going on phase 2 development.</p>
+    <p>This is not only trial but going on phase 2 development.</p>
+  </div>
+</template>
+
+<style scoped>
+.about-page {
+  text-align: center;
+  padding: 30px;
+}
+</style>
